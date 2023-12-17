42239fc28c1b95eb955538f5d0b7547491f08eb9
Marc Bourgoin <themard85@gmail.com>
2023-12-17 15:06:04 -0700

    Z01R Add firmware
    Change-Id: I23485794ba627b63f9ff64da883ec0d2f7b29152

--- Android.mk
+++ Android.mk
@@ -6,4 +6,21 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),Z01R)
 
+$(call add-radio-file-sha1-checked,radio/ImageFv.img,e5861a056f84e660e672f27cc34d07a73bb328a1)
+$(call add-radio-file-sha1-checked,radio/abl.img,dec0a7dc0754b3a22392580b3290edd9ef774c02)
+$(call add-radio-file-sha1-checked,radio/aop.img,a1e0f866b418f4892d2c97419efd5e6dc2fb0cc2)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,331ffee97233728be8092c79845c4f45b04c12fd)
+$(call add-radio-file-sha1-checked,radio/cmnlib.img,10ab5878e1f7b2b6d1e01e1fa1784d665d18d60c)
+$(call add-radio-file-sha1-checked,radio/cmnlib64.img,5465b64058336180dd51b88e3830ba97a6e8d101)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,306ad458428a3b4f9190007a81413de97a135ae4)
+$(call add-radio-file-sha1-checked,radio/dsp.img,ada0a90cb9b73a719f24bcad1a67438197005131)
+$(call add-radio-file-sha1-checked,radio/hyp.img,ce1e6ae17aed0d824cbf024297607a975d222ad9)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,be99ad7ff6a80db38e9d858782794299cca17662)
+$(call add-radio-file-sha1-checked,radio/modem.img,22e0cc691edfa57d81a80696c4c6d00f21c4438e)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,802241390538da3c145584ba0d1432e870d8da3c)
+$(call add-radio-file-sha1-checked,radio/storsec.img,6cf2016ac88c99f8f83360b4a9c17ef49d40f8ba)
+$(call add-radio-file-sha1-checked,radio/tz.img,91cf80cc9b25bdd67c65bbb8225336bd8421d601)
+$(call add-radio-file-sha1-checked,radio/xbl.img,58228bab068f8d8ddf514cb4d0fb72c8c9cd2c83)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,acb9163e5765a0b2945e28c34f104617687cca9f)
+
 endif
